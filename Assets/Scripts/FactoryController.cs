00e0e524a7acd5f3421d1718af221461fcb20d0e
mrKrenar <piraj.krenar@gmail.com>
2021-11-06 19:51:37 +0100

    Fixed earth prfab and added scripts for managing player and factory

---
+++ Assets/Scripts/FactoryController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FactoryController : MonoBehaviour, ILitterable
+{
+    [SerializeField] float timeBetweenGarbageSpawns = 3, minTimeBetweenGarbageSpawns = .1f;
+    [SerializeField] float timeDecreasePerSpawnsUpgrade = .1f;
+    [SerializeField] int spawnsDelayUpgradeCost = 10;
+
+    [SerializeField] float timeOfTrack = 3, minTimeOfTrack = .1f;
+    [SerializeField] float timeDecreasePerTrackSpeedUpgrade = .1f;
+    [SerializeField] int trackSpeedUpgradeCost = 10;
+
+    [SerializeField] int earningsPerGarbageThrown = 1;
+
+    [SerializeField] Transform garbageStartSpawn, garbageEndSpawn;
+
+    public void StartLittering()
+    {
+        StopCoroutine(nameof(StartThrowingGarbage));
+        StartCoroutine(nameof(StartThrowingGarbage));
+    }
+    IEnumerator StartThrowingGarbage()
+    {
+        yield return new WaitForSeconds(timeBetweenGarbageSpawns * Random.Range(.8f, 1.2f));
+        while (true)
+        {
+            ThrowGarbage();
+            yield return new WaitForSeconds(timeBetweenGarbageSpawns);
+        }
+    }
+
+    public void UpgradeThrowDelay()
+    {
+        if (timeBetweenGarbageSpawns - timeDecreasePerSpawnsUpgrade > minTimeBetweenGarbageSpawns && CoinsSystem.RemoveCoins(spawnsDelayUpgradeCost))
+        {
+            timeBetweenGarbageSpawns -= timeDecreasePerSpawnsUpgrade;
+            StartLittering();
+        }
+    }
+
+    public void UpgradeTrackSpeed()
+    {
+        if (timeOfTrack - timeDecreasePerTrackSpeedUpgrade > minTimeOfTrack && CoinsSystem.RemoveCoins(trackSpeedUpgradeCost))
+        {
+            timeOfTrack -= timeDecreasePerTrackSpeedUpgrade;
+            StartLittering();
+        }
+    }
+
+    private void ThrowGarbage()
+    {
+        CoinsSystem.AddCoins(earningsPerGarbageThrown);
+
+        //var garbage = Instantiate(...)
+        //garbage.transform.localScale *= Vector3.one * Random.Range(.8f, 1.2f);
+        //garbage.transform.position = garbageStartSpawn.position;
+
+        //garbage.transform.DOMove(garbageEndSpawn.position, timeOfTrack)
+        //    .OnComplete(()=>
+        //    {
+        //        //garbage.transform.DOPath(new Vector3[] { new Vector3(garbage.transform.position.x, garbage.transform.position.y, garbage.transform.position.z) },)
+        //    });
+
+
+        //on finished, throw to canal
+    }
+}
--- Assets/Scripts/HumanController.cs
+++ Assets/Scripts/HumanController.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HumanController : MonoBehaviour
+public class HumanController : MonoBehaviour, ILitterable
 {
     [SerializeField] Animator animator;
+    [SerializeField] float timeBetweenDrops = 3, minTimeBetweenDrops = .1f;
+    [SerializeField] float timeDecreasePerUpgrade = .1f;
+    [SerializeField] int upgradeCost = 10, earningsPerGarbageThrown = 1;
 
-    private void Update()
+    public void StartLittering()
     {
-        if (Input.anyKeyDown)
+        StopCoroutine(nameof(StartThrowingGarbage));
+        StartCoroutine(nameof(StartThrowingGarbage));
+    }
+    IEnumerator StartThrowingGarbage()
+    {
+        yield return new WaitForSeconds(timeBetweenDrops * Random.Range(.8f, 1.2f));
+        while (true)
         {
-            animator.SetTrigger("tr_ThrowGarbage");
+            ThrowGarbage();
+            yield return new WaitForSeconds(timeBetweenDrops);
         }
+    }
+
+    public void Upgrade()
+    {
+        if (timeBetweenDrops - timeDecreasePerUpgrade > minTimeBetweenDrops && CoinsSystem.RemoveCoins(upgradeCost))
+        {
+            timeBetweenDrops -= timeDecreasePerUpgrade;
+            StartLittering();
+        }
+    }
+
+    private void ThrowGarbage()
+    {
+        animator.SetTrigger("tr_ThrowGarbage");
+        CoinsSystem.AddCoins(earningsPerGarbageThrown);
+        //TODO: continue implementation
     }
 }
