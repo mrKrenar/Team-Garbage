c8748bb258d4d712d73597de9aecc01db3159799
mrKrenar <piraj.krenar@gmail.com>
2021-11-06 10:25:12 +0100

    Added human controller script

---
+++ Assets/Scripts/HumanController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            animator.SetTrigger("tr_ThrowGarbage");
+        }
+    }
+}
