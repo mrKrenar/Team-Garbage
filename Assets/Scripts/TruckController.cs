d57e62354f7ee17813297a287195de746a59ddc1
mrKrenar <piraj.krenar@gmail.com>
2021-11-06 23:14:07 +0100

    Added truck controller

---
+++ Assets/Scripts/TruckController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TruckController : MonoBehaviour
+{
+    [SerializeField] int garbageCount = 10;
+    [SerializeField] int moveSpeed = 10;
+
+
+    public void StartLittering(Vector3 moveTo)
+    {
+        transform.DOMove(moveTo, moveSpeed).OnComplete(() => {
+            ThrowGarbage();
+        });
+    }
+    
+    void ThrowGarbage()
+    {
+        //throw <garbageCount> garbages
+    }
+}
